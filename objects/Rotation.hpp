0d8d8e369d645161d027d198978cfbce0ede3cfb
GuillaumeGomez <guillaume1.gomez@gmail.com>
2014-12-06 01:23:23 +0100

    Indentation of Rotation

--- objects/Rotation.hpp
+++ objects/Rotation.hpp
@@ -6,25 +6,25 @@
 class Rotation
 {
 public:
-  Rotation(float rot_x = 0.f, float speed_x = 0.f, float rot_y = 0.f, float speed_y = 0.f, float rot_z = 0.f, float speed_z = 0.f);
-  Rotation(Rotation const&);
-  virtual ~Rotation(){}
-  float &x();
-  float &y();
-  float &z();
-  float &speedX();
-  float &speedY();
-  float &speedZ();
-  Vector3D  toVector3D() const;
-  void  update(const float &);
+    Rotation(float rot_x = 0.f, float speed_x = 0.f, float rot_y = 0.f, float speed_y = 0.f, float rot_z = 0.f, float speed_z = 0.f);
+    Rotation(Rotation const&);
+    virtual ~Rotation(){}
+    float &x();
+    float &y();
+    float &z();
+    float &speedX();
+    float &speedY();
+    float &speedZ();
+    Vector3D  toVector3D() const;
+    void  update(const float &);
 
 private:
-  float m_rotX;
-  float m_speedX;
-  float m_rotY;
-  float m_speedY;
-  float m_rotZ;
-  float m_speedZ;
+    float m_rotX;
+    float m_speedX;
+    float m_rotY;
+    float m_speedY;
+    float m_rotZ;
+    float m_speedZ;
 };
 
 #endif // ROTATION_HPP
